666e95277c562bbf26b7b547a069ef526b80b7e3
Young Seok Seo <young7seo@gmail.com>
2018-12-31 13:47:06 -0800

    added TTTGame.c

---
+++ TTTGame.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "TTTLib.c"
+
+int main(void){
+
+	int *T;
+	int Xmove = 0;
+	int state = 0;
+	int winmove = 0;
+	int nonlosemove = 0;
+	int randmove = 0;
+
+	T = genBoard();
+
+	while(1){
+
+		/*Player "X" (user)*/
+
+		printf("%d\n", printBoard(T,9));
+		printf("X move?\n");
+		scanf("%d", &Xmove);
+
+		/*place X in the indicated slot*/
+
+		if (T[Xmove] == 0){
+			T[Xmove] = 1;
+		}
+
+		/*analyze the game state after X move*/
+
+		state = analyzeBoard(T,9);
+
+		if (state == 1){
+			printf("%d\n", printBoard(T,9));
+			printf("Player X wins!\n");
+			break;
+		}
+
+		if (state == 3){
+			printf("%d\n", printBoard(T,9));
+			printf("Draw\n");
+			break;
+		}
+
+
+		/*Player "O" (computer)*/
+
+		printf("%d\n", printBoard(T,9));
+
+		/*place O in the best spot as analyzed by the simulation functions*/
+
+		while(1){
+
+			winmove = genWinningMove(T,9,2);
+			if (winmove != -1){
+				T[winmove] = 2;
+				break;
+			}
+
+			nonlosemove = genNonLosingMove(T,9,2);
+			if (nonlosemove != -1){
+				T[nonlosemove] = 2;
+				break;
+			}
+
+			randmove = genRandomMove(T,9,2);
+			if (T[randmove] == 0){
+				T[randmove] = 2;
+				break;
+			}
+		}
+
+		/*analyze the game state after O move*/
+
+		state = analyzeBoard(T,9);
+
+		if (state == 2){
+			printf("%d\n", printBoard(T,9));
+			printf("Player O wins!\n");
+			break;
+		}
+
+		if (state == 3){
+			printf("%d\n", printBoard(T,9));
+			printf("Draw\n");
+			break;
+		}
+	}
+}
